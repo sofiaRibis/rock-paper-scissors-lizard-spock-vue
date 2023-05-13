28ef71a1055b31047f28c393c7982bb911c9c352
sofiaRibis <sofia.ribis@gmail.com>
2023-05-13 11:40:17 +0200

    fix routes path

--- src/router.js
+++ src/router.js
@@ -3,15 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import WelcomePage from "./pages/WelcomePage.vue";
 import GamePage from "./pages/GamePage.vue";
 
-const routes = [
-	{ path: "/", redirect: WelcomePage },
-	{ path: "/welcome", component: WelcomePage },
-	{ path: "/game", component: GamePage },
-];
-
 const router = createRouter({
+	routes: [
+		{ path: "/", redirect: "/welcome", name: WelcomePage },
+		{ path: "/welcome", component: WelcomePage },
+		{ path: "/game", component: GamePage },
+	],
 	history: createWebHistory(),
-	routes,
 });
 
 export default router;
